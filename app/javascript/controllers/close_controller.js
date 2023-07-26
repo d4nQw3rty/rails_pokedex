a2d980c3e195b27d9a8a428f890c1db3b0cf3b19
Danys Linares <danlinmoy@gmail.com>
2023-07-26 17:00:49 -0400

    close

--- app/javascript/controllers/close_controller.js
+++ app/javascript/controllers/close_controller.js
@@ -1,21 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
-  }
-
+ 
   close(){
     e.preventDefault();
 
     // Remove from parent
     const modal = document.getElementById("modal");
     modal.innerHTML="";
-
-    // Remove the src attribute from the modal
-    modal.removeAttribute("src");
-
-    // Remove complete attribute
-    modal.removeAttribute("complete");
   }
 }
